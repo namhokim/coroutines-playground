1bc29d7f8afa5e0dc32dd668c9b69edbda67c4cb
namo <nhkimnamo@gmail.com>
2022-10-15 12:09:45 +0900

    feat(project): init course project

---
+++ course-leonardo/src/main/kotlin/com/github/namhokim/courseleonardo/CourseLeonardoApplication.kt
@@ -0,0 +1,11 @@
+package com.github.namhokim.courseleonardo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CourseLeonardoApplication
+
+fun main(args: Array<String>) {
+    runApplication<CourseLeonardoApplication>(*args)
+}
