f093a98b81692e13d0a6213c848574a4fd555029
namo <nhkimnamo@gmail.com>
2022-10-15 13:14:51 +0900

    feat(part1): 1-1 Scope Builder ex5
    - delay

---
+++ course-leonardo/src/main/kotlin/com/github/namhokim/courseleonardo/ex/ex5.kt
@@ -0,0 +1,14 @@
+package com.github.namhokim.courseleonardo.ex
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    launch {
+        println("launch: ${Thread.currentThread().name}")   // 2
+        // delay (100L) vs delay (600L)
+        println("World!")                                   // 3
+    }
+    println("runBlocking: ${Thread.currentThread().name}")  // 1
+    delay(500L) // vs Thread.sleep(500L)
+    println("Hello")                                        // 4
+}
