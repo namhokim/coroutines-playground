3ff9326325f3a773a121186fde1ed0a8712503fd
namo <nhkimnamo@gmail.com>
2022-10-15 13:24:35 +0900

    feat(part1): 1-1 Scope Builder ex7
    - multi launch with delay

---
+++ course-leonardo/src/main/kotlin/com/github/namhokim/courseleonardo/ex/ex7.kt
@@ -0,0 +1,18 @@
+package com.github.namhokim.courseleonardo.ex
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    launch {
+        println("launch1: ${Thread.currentThread().name}")
+        delay(1000L)
+        println("3!")
+    }
+    launch {
+        println("launch2: ${Thread.currentThread().name}")
+        println("1!")
+    }
+    println("runBlocking: ${Thread.currentThread().name}")
+    delay(500L)
+    println("2!")
+}
