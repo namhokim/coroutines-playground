98cb215c4a131644704e3f7119092d68a758cd02
namo <nhkimnamo@gmail.com>
2022-10-16 22:05:21 +0900

    feat(part1): 1-7 Mutex Actor
    - Problem of shared object

---
+++ course-leonardo/src/main/kotlin/com/github/namhokim/courseleonardo/ex/part1/section7/ex39.kt
@@ -0,0 +1,32 @@
+package com.github.namhokim.courseleonardo.ex.part1.section7
+
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+suspend fun massiveRun(action: suspend () -> Unit) {
+    val n = 100
+    val k = 1000
+    val elapsed = measureTimeMillis {
+        coroutineScope {
+            repeat(n) {
+                launch {
+                    repeat(k) {
+                        action()
+                    }
+                }
+            }
+        }
+    }
+    println("$elapsed ms 동안 ${n * k}개의 연산을 수행")
+}
+
+var counter = 0
+
+fun main() = runBlocking {
+    withContext(Dispatchers.Default) {
+        massiveRun {
+            counter++
+        }
+    }
+    println("Counter = $counter")
+}
