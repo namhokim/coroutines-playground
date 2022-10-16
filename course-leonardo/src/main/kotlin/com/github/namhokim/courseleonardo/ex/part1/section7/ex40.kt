318db82fc605c9d07488cfbbb789771d3c8108fd
namo <nhkimnamo@gmail.com>
2022-10-16 22:08:18 +0900

    feat(part1): 1-7 Mutex Actor
    - Solve with volatile

--- course-leonardo/src/main/kotlin/com/github/namhokim/courseleonardo/ex/part1/section7/ex40.kt
+++ course-leonardo/src/main/kotlin/com/github/namhokim/courseleonardo/ex/part1/section7/ex40.kt
@@ -3,7 +3,7 @@ package com.github.namhokim.courseleonardo.ex.part1.section7
 import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
 
-suspend fun massiveRun(action: suspend () -> Unit) {
+suspend fun massiveRunVolatile(action: suspend () -> Unit) {
     val n = 100
     val k = 1000
     val elapsed = measureTimeMillis {
@@ -20,11 +20,12 @@ suspend fun massiveRun(action: suspend () -> Unit) {
     println("$elapsed ms 동안 ${n * k}개의 연산을 수행")
 }
 
+@Volatile
 private var counter = 0
 
 fun main() = runBlocking {
     withContext(Dispatchers.Default) {
-        massiveRun {
+        massiveRunVolatile {
             counter++
         }
     }
