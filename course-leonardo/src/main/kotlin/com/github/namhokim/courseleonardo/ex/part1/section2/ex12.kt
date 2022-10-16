0b263824d568ec2ceb4101121184809e680b20d7
namo <nhkimnamo@gmail.com>
2022-10-16 17:29:33 +0900

    feat(part1): 1-2 Structured Concurrency
    - Control by Job

---
+++ course-leonardo/src/main/kotlin/com/github/namhokim/courseleonardo/ex/part1/section2/ex12.kt
@@ -0,0 +1,30 @@
+package com.github.namhokim.courseleonardo.ex.part1.section2
+
+import kotlinx.coroutines.*
+
+suspend fun doOneTwoThree12() = coroutineScope {
+    val job: Job = launch {
+        println("launch1: ${Thread.currentThread().name}")
+        delay(1500L)
+        println("3!")
+    }
+    job.join()
+
+    launch {
+        println("launch2: ${Thread.currentThread().name}")
+        println("1!")
+    }
+    launch {
+        println("launch3: ${Thread.currentThread().name}")
+        delay(500L)
+        println("2!")
+    }
+    println("4!")
+}
+
+fun main() = runBlocking {
+    doOneTwoThree12()
+    println("runBlocking: ${Thread.currentThread().name}")
+    delay(100L)
+    println("5!")
+}
